ffa1ec14b9f77e6e5176c8723b72aceddb8399a4
gitDbits <dbs.noronha@gmail.com>
2021-08-20 23:16:03 -0400

    Fix schema db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_12_165546) do
+ActiveRecord::Schema.define(version: 2021_08_20_235302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2021_06_12_165546) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "birthday"
+    t.string "phone", limit: 20
+    t.string "street"
+    t.string "number", limit: 20
+    t.string "neighborhood"
+    t.string "zip_code", limit: 20
   end
 
 end
