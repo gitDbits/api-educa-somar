a92d4d831ddb669ac0b0d90746b0beb10ccc86b1
gitDbits <dbs.noronha@gmail.com>
2021-09-10 11:57:20 -0400

    Added attritube cpf for factory user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
+    cpf { Faker::IDNumber.brazilian_cpf }
     password { 'password' }
     password_confirmation { 'password' }
 
