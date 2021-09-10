5519da516b3201f86fa95144b80441641ea85541
gitDbits <dbs.noronha@gmail.com>
2021-09-10 12:10:49 -0400

    Added friendly_id to user and valid uniqueness CPF to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   require 'cpf_cnpj'
+
+  extend FriendlyId
+  friendly_id :cpf, use: :slugged
+
   has_secure_password
 
   belongs_to :state
@@ -8,7 +12,8 @@ class User < ApplicationRecord
   validates_with CpfValidator
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :cpf, presence: true, uniqueness: true
   validates :password,
-            length: { minimum: 6 },
-            if: -> { new_record? || !password.nil? }
+  length: { minimum: 6 },
+  if: -> { new_record? || !password.nil? }
 end
