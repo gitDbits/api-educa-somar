2ac0488c5ecb7f3af6e08a6a156ef65ca758f3ed
gitDbits <dbs.noronha@gmail.com>
2021-09-10 13:06:02 -0400

    Added validate presence for fields user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :cpf, presence: true, uniqueness: true
+  validates :street, presence: true
+  validates :number, presence: true 
+  validates :neighborhood, presence: true 
+  validates :zip_code, presence: true 
   validates :password,
   length: { minimum: 6 },
   if: -> { new_record? || !password.nil? }
